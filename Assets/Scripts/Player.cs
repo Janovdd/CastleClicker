5c2abd3af9bea804378a71d7f6d350fce49b6289
Unknown <pspdonny@live.com>
2018-01-29 16:57:42 +0100

    Troop
    Dank troops

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	
+	public GameObject troop;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)) {
+		
+			Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+			RaycastHit raycastHit;
+		
+			if (Physics.Raycast(raycast, out raycastHit)) {
+				if (raycastHit.collider.CompareTag("playerCastle")) {
+					Debug.Log("Soccer Ball clicked");
+				}
+			}
+		}
+		
+		if(Input.GetMouseButtonDown(0)){
+			Instantiate(troop); 
+		}
+		
+	}
+	
+}
---
+++ Assets/Scripts/Troop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Troop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.right * Time.deltaTime;
+	}
+}
