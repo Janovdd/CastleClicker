102053ec0c9f52699a485b3e1b0de4c388c5c2de
Janovdd <janovdd@gmail.com>
2018-02-01 21:29:26 +0100

    add random ZPosition

---
+++ Assets/ZPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZPosition : MonoBehaviour {
+
+    float x;
+    float y;
+    int z;
+    Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+        x = -6.8f;
+        y = -6.8f;
+        z = Random.Range(0, -10);
+        pos= new Vector3(x, y, z);
+        transform.position = pos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
